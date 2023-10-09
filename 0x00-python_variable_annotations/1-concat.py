0c5db10426f59adc86fd811a25efad60320e368d
pacmanlolo <michael.omotosho7@gmail.com>
2023-10-09 10:15:47 +0000

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+ type-annotated function concat that
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    Concatenates two strings.
+    '''
+    return str1 + str2
