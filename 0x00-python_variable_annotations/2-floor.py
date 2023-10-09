aafe71a45d9adbe53ae9869b234c0fa58791b8f4
pacmanlolo <michael.omotosho7@gmail.com>
2023-10-09 10:17:03 +0000

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+ function floor which takes a float n as argument.
+'''
+
+
+def floor(a: float) -> int:
+    '''Computes the floor of a floating-point number.
+    '''
+    return int(a)
